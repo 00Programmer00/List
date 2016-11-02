c3935749c1b0eaef01755910c6e74293e14e3ab9
Programer <dahnenko02501@gmail.com>
2016-11-02 19:30:35 +0200

    Update JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
-    var btn1 = document.getElementById("Edit");
-    var btn2 = document.getElementById("Remove");
-    var btn3 = document.getElementById("Add");
+    var btn1 = document.getElementById("edit");
+    var btn2 = document.getElementById("Add");
     var list = document.getElementById("List");
     var item = document.getElementById("input");
     var listItem = document.getElementsByClassName("item");
-    console.log(listItem[3]);
+    var editBox = document.getElementById("editBox");
+    var cross = document.getElementsByClassName("image");
 
-    btn3.onclick = function (event) {
+    btn2.onclick = function (event) {
         var inp = item.value;
         var newCheck = document.createElement("input");
         newCheck.setAttribute("class","checkbox");
@@ -17,40 +17,62 @@
         newItem.innerHTML = inp;
         newItem.setAttribute("class","item");
         list.appendChild(newItem);
+        var createX = document.createElement("img");
+        createX.setAttribute("src","images/2.png");
+        createX.setAttribute("class","image");
+        newItem.appendChild(createX);
     }
-    btn2.onclick = function (event) {
-        var inp = item.value;
-        for(var i = 0;i<listItem.length;i++) {
-            if (inp === listItem[i]) {
-                list.removeChild(inp);
+
+    function deleteItem(el) {
+        el.parentNode.removeChild(el);
+    }
+
+    btn1.onclick = function (event) {
+        var button =  document.createElement("button");
+        button.innerHTML="Change color";
+        button.setAttribute("class","editButton");
+        var button1 =  document.createElement("button");
+        button1.innerHTML="Set font size";
+        button1.setAttribute("class","editButton");
+        var button2 =  document.createElement("button");
+        button2.innerHTML="Change list text";
+        button2.setAttribute("class","editButton");
+        editBox.appendChild(button);
+        editBox.appendChild(button1);
+        editBox.appendChild(button2);
+
+        button.onclick = function (event) {
+            for (var i = 0; i < listItem.length; i++) {
+                listItem[i].style.color = getRandomColor();
             }
         }
+
+        button1.onclick = function (event) {
+            var question = prompt("Set font-size");
+            for (var i = 0; i < listItem.length; i++) {
+                listItem[i].style.fontSize = question + "px";
+
+            }
+
+        }
+
+        button2.onclick = function (event) {
+            var question = prompt("Choose number of item which you want to change(first item equal 0)");
+            var selectedItem = listItem[+question];
+            selectedItem.innerHTML = "";
+            var secondQuestion = prompt("Write new text");
+            selectedItem.innerHTML = secondQuestion;
+            selectedItem.setAttribute("class","item");
+            }
+        }
+
+
+    function getRandomColor() {
+        return "#" + ((1 << 24) * Math.random() | 0).toString(16);
     }
 
-    listItem.onmouseover = function (event) {
-        listItem[1].style.left = 50 + "px";
-    }
 
 
 
 
 
-
-    /*function addEl() {
-        var newItem = document.createElement("li");
-        newItem.innerHTML = inp;
-        list.appendChild(newItem);
-        newItem.setAttribute("class",item);
-    }*/
-
-
-    /*function chPos() {
-        item.style.left = "300px";
-    }
-    btn1.onclick = function(event){
-        setPosition(item,30,0);
-    }
-    function setPosition(element, x, y) {
-        element.style.left = x + "px";
-        element.style.top = y + "px";
-    }*/
